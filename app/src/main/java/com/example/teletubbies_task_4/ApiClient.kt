b0a8f5a30eb7647adcb8f99da1566c5edc6230da
yehiaa <esc.yehia.mohamed1@gmail.com>
2020-08-31 00:06:24 +0200

    Added ApiClient.kt

---
+++ app/src/main/java/com/example/teletubbies_task_4/ApiClient.kt
@@ -0,0 +1,20 @@
+package com.example.teletubbies_task_4
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    private var instance: Retrofit? = null
+
+    fun getClient(): Retrofit {
+        if (instance == null)
+            instance = Retrofit.Builder()
+                .baseUrl("Enter Your API's URL Here")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(OkHttpClient().newBuilder().build())
+                .build()
+
+        return instance!!
+    }
+}
