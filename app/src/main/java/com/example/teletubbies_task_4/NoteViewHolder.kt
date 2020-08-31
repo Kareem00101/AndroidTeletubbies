2de3c4ce389e4a7ea9da052b11b620fbfb665ab0
Aliaa Karam <aliaakaram337@yahoo.com>
2020-08-31 16:39:24 +0200

    class NoteViewHolder is done

--- app/src/main/java/com/example/teletubbies_task_4/NoteViewHolder.kt
+++ app/src/main/java/com/example/teletubbies_task_4/NoteViewHolder.kt
@@ -1,8 +1,15 @@
 package com.example.teletubbies_task_4
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_note.view.*
 
 class NoteViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView) {
-
+    val note_posterPortrait :ImageView = itemView.posterPortrait
+    val note_title : TextView = itemView.title
+    val note_rating : TextView = itemView.rating
+    val note_language : TextView = itemView.language
+    val note_release : TextView = itemView.release
 }
