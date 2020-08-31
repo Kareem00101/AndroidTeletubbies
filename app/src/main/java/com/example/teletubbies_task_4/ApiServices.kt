7c6bef56c0db1c862194a0aba327ba79f1b39ec5
Malak Madkour <malookamiro@gmail.com>
2020-08-31 09:21:34 +0200

    Added configuration and its data class fot the image path.

--- app/src/main/java/com/example/teletubbies_task_4/ApiServices.kt
+++ app/src/main/java/com/example/teletubbies_task_4/ApiServices.kt
@@ -5,12 +5,22 @@ import retrofit2.http.Query
 
 interface ApiServices {
 
-    @GET("popular")
+    @GET("/movie/popular")
     fun getMovie(
-        @Query("api key") apiKey: String,
+        @Query("api_key") apiKey: String,
         /*@Query("language") language: String,         //These 2 are optional.
         @Query("page") page: Int*/
     )
+
+    @GET("/configuration")
+    fun config (
+        @Query("api_key") apiKey: String,
+    )
 }
 
+//Popular Movie
 //https://api.themoviedb.org/3/movie/popular?api_key=4b7ad36f69f80aa34703d042a53836e4&language=en-US&page=1
+
+//Configuration fot the image path
+//https://api.themoviedb.org/3/configuration?api_key=4b7ad36f69f80aa34703d042a53836e4
+//4b7ad36f69f80aa34703d042a53836e4
--- app/src/main/java/com/example/teletubbies_task_4/MovieResponse.kt
+++ app/src/main/java/com/example/teletubbies_task_4/MovieResponse.kt
@@ -38,7 +38,30 @@ data class MovieDetails(
     @SerializedName("adult")               //We can make a message or an adult only icon to appear when true
     val adult: Boolean
 
+)
 
+// This class is for the configuration API which I added so that the poster and backdrop paths are cpmplete.
 
+data class ImageDetails(
+    @SerializedName("secure_base_url")
+    val baseURL: String,
+    @SerializedName("backdrop_sizes")
+    val backdropSize: List<String>,
+    @SerializedName("poster_sizes")
+    val posterSize: List<String>
 
 )
+
+/*To whoever is making the main:
+
+You'll notice that movie, TV and person objects contain references to different file paths.
+In order to generate a fully working image URL, you'll need 3 pieces of data.
+Those pieces are a base_url, a file_size and a file_path.
+The first two pieces can be retrieved by calling the  API and the third is the file path you're wishing to grab on a particular media object.
+ Here's what a full image URL looks like if the poster_path of /kqjL17yufvn9OVLyXYpvtyrFfak.jpg was returned for a movie, and you were looking for the w500 size:
+
+https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
+
+
+
+* */
