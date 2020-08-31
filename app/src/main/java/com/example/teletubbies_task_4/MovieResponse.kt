5896e2804612f870c094ce47820a7a0424bf5c28
Malak Madkour <malookamiro@gmail.com>
2020-08-31 07:37:00 +0200

    Added extra data for the bonus screen.

--- app/src/main/java/com/example/teletubbies_task_4/MovieResponse.kt
+++ app/src/main/java/com/example/teletubbies_task_4/MovieResponse.kt
@@ -15,17 +15,29 @@ data class MovieResponse(
 
 
 data class MovieDetails(
-
+//Main Data
     @SerializedName("title")               //Movie Name
     val title: String,
     @SerializedName("poster_path")         //Movie Picture in screen1
     val posterPortrait: String,
     @SerializedName("vote_average")
-    val rating: Float,
-    @SerializedName("original_language")
+    val rating: Double,
+    @SerializedName("original_language")   //we should add a Language: next to it
     val language: String,
     @SerializedName("release_date")
     val release: String,
+
+//Data for the bonus
+
+    @SerializedName("backdrop_path")       //Movie Picture in screen 2 after selecting Movie to view description
+    val pictureLandscape: String,
+    @SerializedName("overview")
+    val description: String,
+    @SerializedName("vote_count")
+    val votes: Int,
+    @SerializedName("adult")               //We can make a message or an adult only icon to appear when true
+    val adult: Boolean
+
 
 
 
