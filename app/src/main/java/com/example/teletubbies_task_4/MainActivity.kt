58fed9239271adb2ae20ff58c8f2fe55460e2819
Code Kareem <kareemkimo575@gmail.com>
2020-09-01 11:16:30 +0200

    Merged both Aliaa and Malak branch into mine.

--- app/src/main/java/com/example/teletubbies_task_4/MainActivity.kt
+++ app/src/main/java/com/example/teletubbies_task_4/MainActivity.kt
@@ -10,13 +10,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-textView_signUp.setOnClickListener {
-   val intent = Intent(this@MainActivity, MainActivity2::class.java)
-    startActivity(intent)
-}
+
+
+        textView_signUp.setOnClickListener {
+        val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
        button_Preview.setOnClickListener {
             val intent = Intent(this@MainActivity, recyclerview_movies::class.java)
             startActivity(intent)
         }
+
     }
 }
