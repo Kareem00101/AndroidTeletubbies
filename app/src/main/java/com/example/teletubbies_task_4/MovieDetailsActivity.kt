9083dbe44e11e85eef3be4518630663e671f52e9
Code Kareem <kareemkimo575@gmail.com>
2020-09-03 09:24:58 +0200

    Created test environment.

--- app/src/main/java/com/example/teletubbies_task_4/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/teletubbies_task_4/MovieDetailsActivity.kt
@@ -2,11 +2,33 @@ package com.example.teletubbies_task_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.item_note.*
+import kotlinx.android.synthetic.main.movie_details.*
 
-class MovieDetailsActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity(), MovieRepository.MovieCallBack  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.movie_details)
-        println("Hello World")
+
+        requestMoviesData("English")
+
+    }
+    private fun requestMoviesData(movieLang: String = "English")
+    {
+
+        MovieRepository.requestMovieData(movieLang, this)
+    }
+    private fun bindMoviesData(movie: MovieResponse)
+    {
+        movieOverviewText.text=movie.resultsList[0].description
+    }
+
+    override fun onMovieReady(movie: MovieResponse) {
+        bindMoviesData(movie)
+    }
+
+    override fun onMovieError(errorMsg: String) {
+        Toast.makeText(this@MovieDetailsActivity, errorMsg, Toast.LENGTH_LONG).show()
     }
 }
