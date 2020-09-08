2238adba5799f5b3ec16bf1be35df4b3e575b1d6
Nardine2 <nardineyousry2@gmail.com>
2020-09-08 21:36:36 +0200

    MVM class

--- app/src/main/java/com/example/teletubbies_task_4/MainViewModel.kt
+++ app/src/main/java/com/example/teletubbies_task_4/MainViewModel.kt
@@ -1,8 +1,44 @@
 package com.example.teletubbies_task_4
 
-
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel(), MovieRepository.MovieCallBack {
 
+    private val _movieLiveData: MutableLiveData<MovieResponse>
+            by lazy { MutableLiveData() }
+    val movieLiveData: LiveData<MovieResponse>
+        get() = _movieLiveData
+
+    private val _onError: MutableLiveData<String>
+            by lazy { MutableLiveData() }
+    val onError: LiveData<String>
+        get() = _onError
+
+    private lateinit var movieData: MovieResponse
+
+    private var currentMovieName = "Title"
+
+    fun loadMovieData(movieName: String = "") {
+
+        if (movieName == currentMovieName && this::movieData.isInitialized) {
+            _movieLiveData.value = movieData
+            return
+        }
+
+        if (movieName.isNotEmpty())
+            currentMovieName = movieName
+
+        MovieRepository.requestMovieData(currentMovieName, this)
+    }
+
+    override fun onMovieReady(weather: MovieResponse) {
+        movieData = weather
+        _movieLiveData.value = movieData
+    }
+
+    override fun onMovieError(errorMsg: String) {
+        _onError.value = errorMsg
+    }
 }
