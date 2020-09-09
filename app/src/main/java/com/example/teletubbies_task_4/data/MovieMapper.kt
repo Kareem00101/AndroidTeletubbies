b0ec537ef1560f23f1027ec736b640dc11a9e7c8
Aliaa Karam <aliaakaram337@yahoo.com>
2020-09-09 12:55:24 +0200

    MovieMapper.kt Class is Done

---
+++ app/src/main/java/com/example/teletubbies_task_4/data/MovieMapper.kt
@@ -0,0 +1,22 @@
+package com.example.teletubbies_task_4.data
+
+import com.example.teletubbies_task_4.data.models.remote.MovieResponse
+
+class MovieMapper {
+
+        fun mapToMovieUi(movieResponse: MovieResponse): List<Movie> {
+        val Movies = mutableListOf<Movie>()
+            var title = ""
+            var posterPortrait = ""
+            var rating = ""
+            var language = ""
+            var release =""
+            var description = ""
+
+        movieResponse.resultsList.forEach {
+
+            Movies.add(Movie(it.title, it.posterPortrait, it.rating, it.language,it.release, it.description))
+        }
+        return Movies
+    }
+}
