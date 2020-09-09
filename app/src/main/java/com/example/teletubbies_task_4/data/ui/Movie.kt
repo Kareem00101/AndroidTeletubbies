4d95ec008a369df943cb5cc2452c9aa2a9ca5eb0
Malak Madkour <malookamiro@gmail.com>
2020-09-09 22:42:41 +0200

    Updated Movie.kt in preparation for the database. Dependency not added yet.

--- app/src/main/java/com/example/teletubbies_task_4/data/ui/Movie.kt
+++ app/src/main/java/com/example/teletubbies_task_4/data/ui/Movie.kt
@@ -1,6 +1,12 @@
 package com.example.teletubbies_task_4.data.ui
 
+//Insert dependencies then remove the // in the comments below
+
+
+//@Entity(tableName = "Movie_table")
 data class Movie (
+    //@PrimaryKey
+    val id: Long,
     val title: String,
     val posterPortrait: String,
     val rating: Double,
