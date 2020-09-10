1948ecf2325240d32522baf98c73fd5c87ce1b6a
Code Kareem <kareemkimo575@gmail.com>
2020-09-10 06:45:59 +0200

    Added empty app Database class.

---
+++ app/src/main/java/com/example/teletubbies_task_4/data/database/AppDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.teletubbies_task_4.data.database
+
+import androidx.room.Database
+import com.example.teletubbies_task_4.data.ui.Movie
+
+
+@Database(entities = [Movie::class], version = 1)
+class AppDatabase
+{
+
+}
