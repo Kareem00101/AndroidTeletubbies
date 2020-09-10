61ed1470a560d2dc4b5eafa04442806bb2f172d7
Code Kareem <kareemkimo575@gmail.com>
2020-09-10 06:54:51 +0200

    Added modification and notes to app database.

--- app/src/main/java/com/example/teletubbies_task_4/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/teletubbies_task_4/data/database/AppDatabase.kt
@@ -1,11 +1,17 @@
 package com.example.teletubbies_task_4.data.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.teletubbies_task_4.data.ui.Movie
 
-
-@Database(entities = [Movie::class], version = 1)
-class AppDatabase
+//TODO: Implement this class.
+@Database(entities = [Movie::class], version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase()
 {
+    //Implement abstract function.
+    //Instance creation implementation.
+    //Remember Threading.
+    //Create companion object for the abstract class.
+    //The database is the one who implements the abstract functions.
 
 }
