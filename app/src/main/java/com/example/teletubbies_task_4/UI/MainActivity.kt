01acf3138c6ff772d13527449f21a4256e88dcce
yehiaa <esc.yehia.mohamed1@gmail.com>
2020-09-10 09:04:55 +0200

    Modified ( activity_main2.xml and item_note.xml ) View IDs and Constraints.

--- app/src/main/java/com/example/teletubbies_task_4/UI/MainActivity.kt
+++ app/src/main/java/com/example/teletubbies_task_4/UI/MainActivity.kt
@@ -14,12 +14,12 @@ class MainActivity : AppCompatActivity(){
         setContentView(R.layout.activity_main)
 
         //switches you to the sign up layout
-        movie_l8.setOnClickListener {
+        Sign_Up.setOnClickListener {
         val intent = Intent(this@MainActivity, MainActivity2::class.java)
             startActivity(intent)
         }
         //opens the activity that shows you the APP
-       movie_l9.setOnClickListener {
+       Preview_App.setOnClickListener {
             val intent = Intent(this@MainActivity, RecyclerViewMovies::class.java)
             startActivity(intent)
         }
