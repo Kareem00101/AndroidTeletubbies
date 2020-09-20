2960449428b7cec79503204360c114a9006e0b84
Malak Madkour <malookamiro@gmail.com>
2020-09-21 01:08:18 +0200

    Added Functions in Top_RatedFragment.kt.

--- app/src/main/java/com/example/teletubbies_task_4/Fragments/Top_RatedFragment.kt
+++ app/src/main/java/com/example/teletubbies_task_4/Fragments/Top_RatedFragment.kt
@@ -5,19 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 
 
 import com.example.teletubbies_task_4.R
+import com.example.teletubbies_task_4.UI.MainViewModel
+import com.example.teletubbies_task_4.UI.MovieAdapter
+import com.example.teletubbies_task_4.data.ui.Movie
+import kotlinx.android.synthetic.main.fragment_popularr.*
 
 class Top_RatedFragment : Fragment() {
 
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view =inflater.inflate(R.layout.fragment_top__rated, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top__rated, container, false)
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        super.onViewCreated(view, savedInstanceState)
+
+        //MVVM PART START HERE
+        mainViewModel.movieLiveData
+            .observe(viewLifecycleOwner, {
+                //button_Preview.isEnabled = true
+                bindMoviesDataWithAdapter(it)
+            })
+
+        mainViewModel.onError.observe(viewLifecycleOwner, {
+            handleMovieError(it)
+        })
+
+        mainViewModel.loadTopRatedMovieData()
+        //MVVM PART ENDS HERE
+    }
+    //This functions links data source with the adapter.
+    private fun bindMoviesDataWithAdapter(movie: List<Movie>)
+    {
+        //Designing recycler view and linking it to the adapter.
+        main_recycler.layoutManager = LinearLayoutManager(activity,
+            LinearLayoutManager.VERTICAL, false )
+        main_recycler.adapter = MovieAdapter(movie)
+    }
+
+    //New one instead of the interface.
+    private fun handleMovieError(errorMsg: String) {
+
+        Toast.makeText(activity, errorMsg, Toast.LENGTH_LONG).show()
+    }
+
+
+
 }
+
