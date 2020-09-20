cc0f273ecdc3680a07f11237fbd57ed2072e1294
Code Kareem <kareemkimo575@gmail.com>
2020-09-21 01:04:51 +0200

    Added few more comments and tested the code.

--- app/src/main/java/com/example/teletubbies_task_4/data/MovieMapper.kt
+++ app/src/main/java/com/example/teletubbies_task_4/data/MovieMapper.kt
@@ -6,6 +6,7 @@ import java.util.*
 
 
 class MovieMapper {
+    //a linked list variable in order to save the old list and add the new after it.
     var joinedList = LinkedList<Movie>()
         fun mapToMovieUi(movieResponse: MovieResponse): List<Movie> {
         val Movies = mutableListOf<Movie>()
@@ -20,8 +21,9 @@ class MovieMapper {
 
             Movies.add(Movie(it.id,it.title, it.posterPortrait, it.rating, it.language,it.release, it.description))
         }
-
+            //adds the new list after the older version.
             joinedList.addAll(Movies)
-        return joinedList
+            //returing the linked list for the whole code to deal with.
+            return joinedList
     }
 }
