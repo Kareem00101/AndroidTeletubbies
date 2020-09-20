b6ffcff726fc1f33bf052be340ec610bc29cfc5b
Nardine2 <nardineyousry2@gmail.com>
2020-09-20 07:43:23 +0200

    Fragment part

--- app/src/main/java/com/example/teletubbies_task_4/UI/RecyclerViewMovies.kt
+++ app/src/main/java/com/example/teletubbies_task_4/UI/RecyclerViewMovies.kt
@@ -4,23 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.teletubbies_task_4.Fragments.FavouriteFragment
+import com.example.teletubbies_task_4.Fragments.SettingsFragment
+import com.example.teletubbies_task_4.Fragments.Top_RatedFragment
 import com.example.teletubbies_task_4.R
-import com.example.teletubbies_task_4.data.models.remote.MovieDetails
-import com.example.teletubbies_task_4.data.models.remote.MovieResponse
 import com.example.teletubbies_task_4.data.ui.Movie
 import kotlinx.android.synthetic.main.activity_recyclerview_movies.*
-import kotlinx.android.synthetic.main.item_note.*
 
 //This is the activity that contains the recycler view.
 class RecyclerViewMovies : AppCompatActivity() {
     private val mainViewModel: MainViewModel by viewModels()
 
 
+// fragment part for the menu bar
+    private val settingsFragment = SettingsFragment()
+    private val favouriteFragment= FavouriteFragment()
+    private val topRatedfragment= Top_RatedFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recyclerview_movies)
+        replaceFragment(settingsFragment)
 
         //MVVM PART START HERE
         mainViewModel.movieLiveData
@@ -35,6 +41,17 @@ class RecyclerViewMovies : AppCompatActivity() {
 
         mainViewModel.loadMovieData()
         //MVVM PART ENDS HERE
+
+
+//fragments
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.Settings -> replaceFragment(settingsFragment)
+                R.id.favourite -> replaceFragment(favouriteFragment)
+                R.id.TopRated -> replaceFragment(topRatedfragment)
+            }
+            true
+        }
 
     }
 
@@ -53,4 +70,12 @@ class RecyclerViewMovies : AppCompatActivity() {
         Toast.makeText(this@RecyclerViewMovies, errorMsg, Toast.LENGTH_LONG).show()
     }
 
+    //FRAGMENT
+    private fun replaceFragment(fragment: Fragment){
+        if (fragment !=null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container,fragment)
+            transaction.commit()
+        }
+    }
 }
